f58a22af26e29312a55d57e1475d7c4231761395
khanhngu123 <94745960+khanhngu123@users.noreply.github.com>
2021-11-28 18:50:23 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,13 +1,13 @@
 const textConfig = {
-  text1: "He luu Dũng",
+  text1: "Xin chào cục cức",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
   text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm chồng tớ rùi đó nha :v",
+  text4: "Nếu cậu ko trả lời mà thoát ra là tao bắn chết cậu đó:v",
   text5: "Cậu mơ à???",
   text6: "Yêu ơi là yêu <3",
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu dễ thương vl moa moa :<<<<<<<<<<<<<<",
+  text9: "Vì cậu học giỏi xinh đẹp lại còn tốt bụng nữa iu lắm moa moa:<<<<<<<<<<<<<<",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
   text11:
     "Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
